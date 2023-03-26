9c8d8a8b6fb8bbf35d5f8250a34c3111b3797f21
KhersonV <khersonv110990@gmail.com>
2023-03-25 21:25:00 -0600

    my-project-2

--- src/components/ArticleList/ArticleListItem.js
+++ src/components/ArticleList/ArticleListItem.js
@@ -7,43 +7,43 @@ import black from "../../img/heart-bl.svg"
 import Reviews from "../Reviews/Reviews";
 
 const ArticleListItem = () => {
-    const productsLikeState = useSelector((state)=>state.productsLikeState)
+  const productsLikeState = useSelector((state) => state.productsLikeState)
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const articleId = new URLSearchParams(window.location.search).get("id"); 
+  const articleId = new URLSearchParams(window.location.search).get("id");
 
-    const article = articlesArray.find((article) => article.id.toString() === articleId);
+  const article = articlesArray.find((article) => article.id.toString() === articleId);
 
-    return (
-        <>
-            
-                
-                    <div className="itemContainer">
-                        <h2 className="category">{article.category}</h2>
-                        <h3 className="title">{article.title}</h3>
-                        <p className="description">{article.description}</p>
-                        <p className="description">  {article.article}</p>
-                        <button
-                className="likeBtn"
-                onClick={() => {
-                  productsLikeState[article.id]
-                    ? dispatch(removeLike(article.id))
-                    : dispatch(addLike(article.id));
-                }}
-              >
-                <img
-                  className="likeBtn-img"
-                  src={productsLikeState[article.id] ? red : black}
-                  alt=""
-                />
-              </button>
-                    </div>
+  return (
+    <>
 
-                    <Reviews/>
-                
-            
-        </>
-    )
+
+      <div className="itemContainer">
+        <h2 className="category">{article.category}</h2>
+        <h3 className="title">{article.title}</h3>
+        <p className="description">{article.description}</p>
+        <p className="description">  {article.article}</p>
+        <button
+          className="likeBtn"
+          onClick={() => {
+            productsLikeState[article.id]
+              ? dispatch(removeLike(article.id))
+              : dispatch(addLike(article.id));
+          }}
+        >
+          <img
+            className="likeBtn-img"
+            src={productsLikeState[article.id] ? red : black}
+            alt=""
+          />
+        </button>
+      </div>
+
+      <Reviews />
+
+
+    </>
+  )
 }
 export default ArticleListItem
